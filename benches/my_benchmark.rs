3cf8f06b3283e378919293ed9ac370444ee0537a
Unknown <haratykflorent@gmail.com>
2019-06-17 21:54:07 +0200

    Update

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -3,10 +3,44 @@ extern crate criterion;
 
 use criterion::Criterion;
 
-fn benchmarked(s: String) {}
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self, BufRead, BufWriter};
+use std::collections::HashMap;
+use std::process::exit;
+
+fn bench(s: &str) {
+    let mut file = File::open(s).unwrap();
+    let mut input = String::new();
+    file.read_to_string(&mut input).unwrap();
+    let input: Vec<String> = input.lines().map(|l| l.to_string()).collect();
+
+    let stdout = io::stdout();
+    let mut output = BufWriter::new(stdout);
+    let mut program = vec![""; input.len()];
+
+    for line in input.iter() {
+        let split: Vec<&str> = line.split_whitespace().collect();
+        let n = split[0].parse::<usize>().unwrap();
+        program[n / 10 - 1] = line;
+    }
+
+    for line in program {
+        let split: Vec<&str> = line.split_whitespace().collect();
+        match split[0] {
+            "IF" => {},
+            "LET" => {},
+            "PRINT" => {},
+            "PRINTLN" => {},
+            _ => {},
+        }
+    }
+}
 
 fn benchmark(c: &mut Criterion) {
-    c.bench_function("bench", |b| b.iter(|| benchmarked("0 1 2 2 2 7".to_string())));
+    c.bench_function("bench", |b| {
+        b.iter(|| bench("F:\\Dev\\Rust\\kattis\\test\\basicinterpreter\\1.in"))
+    });
 }
 
 criterion_group!(benches, benchmark);
